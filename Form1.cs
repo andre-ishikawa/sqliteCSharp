a2fbdf4c7b18d134400dba0505800c8c8e19964d
Andre Ishikawa <andremishikawa@yahoo.com.br>
2021-03-02 16:48:57 -0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CShp_MAC.Models;
+
+namespace CShp_MAC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DalHelper.CriarBancoSQLite();
+            button1.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DalHelper.CriarTabelaSQlite();
+            button2.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ExibirDados();
+        }
+
+        private void ExibirDados()
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+                dt = DalHelper.GetClientes();
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro : " + ex.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!Valida())
+            {
+                MessageBox.Show("Informe os dados cliente a incluir");
+                return;
+            }
+            try
+            {
+                Cliente cli = new Cliente();
+                cli.Id = Convert.ToInt32(textBox1.Text);
+                cli.Nome = textBox2.Text;
+                cli.Email = textBox3.Text;
+
+                DalHelper.Add(cli);
+
+                ExibirDados();
+                LimpaDados();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro : " + ex.Message);
+            }
+        }
+
+        private void LimpaDados()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private bool Valida()
+        {
+            if (string.IsNullOrEmpty(textBox1.Text) && string.IsNullOrEmpty(textBox2.Text) && string.IsNullOrEmpty(textBox3.Text))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (!Valida())
+            {
+                MessageBox.Show("Informe os dados cliente a atualizar");
+                return;
+            }
+
+            try
+            {
+                Cliente cli = new Cliente();
+                cli.Id = Convert.ToInt32(textBox1.Text);
+                cli.Nome = textBox2.Text;
+                cli.Email = textBox3.Text;
+
+                DalHelper.Update(cli);
+                ExibirDados();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro : " + ex.Message);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Informe o ID do cliente a ser Excluído");
+                return;
+            }
+            try
+            {
+                int codigo = Convert.ToInt32(textBox1.Text);
+                DalHelper.Delete(codigo);
+                ExibirDados();
+                LimpaDados();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro : " + ex.Message);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("Informe o ID do cliente a ser Localizado");
+                return;
+            }
+            try
+            {
+                DataTable dt = new DataTable();
+                int codigo = Convert.ToInt32(textBox1.Text);
+
+                dt = DalHelper.GetCliente(codigo);
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro : " + ex.Message);
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            const string mensagem = "Deseja Encerrar ?";
+            const string titulo = "Encerrar";
+            var resultado = MessageBox.Show(mensagem, titulo,
+                                         MessageBoxButtons.YesNo,
+                                         MessageBoxIcon.Question);
+            if (resultado == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void dgvDados_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = this.dataGridView1.Rows[e.RowIndex];
+                textBox1.Text = row.Cells["Id"].Value.ToString();
+                textBox2.Text = row.Cells["Nome"].Value.ToString();
+                textBox3.Text = row.Cells["Email"].Value.ToString();
+            }
+        }
+
+
+    }
+}
